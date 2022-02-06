19f526d9d3ed52b3630b4afbdacf0e8f5d8bae34
Rafael Siqueira <rafael.siqueira@onesight.com.br>
2022-02-06 20:21:05 -0300

    feat: add highcharts

--- src/components/Companies/Companies.tsx
+++ src/components/Companies/Companies.tsx
@@ -1,45 +1,64 @@
+import { useRef } from "react";
 import { useSelector } from "react-redux";
 import { Navigate, useParams } from "react-router-dom";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
 import { ReducerTypes } from "../../reducers/types";
-// import { CompaniesTypes } from "./Companies.types";
 
 const Companies = () => {
-  const { companyId } = useParams();
+  const params = useParams();
+  const companyId = parseInt(params.companyId ?? "0");
   const company = useSelector((state: ReducerTypes) =>
-    state.companies.find((c) => c.id.toString() === companyId)
+    state.companies.find((c) => c.id === companyId)
   );
 
   if (!company) {
     return <Navigate to="/" />;
   }
 
-  return (
-    <>
-      <CompanyDashboard companyId={companyId!.toString()} />
-    </>
-  );
+  return <CompanyDashboard companyId={companyId} />;
 };
 
 export default Companies;
 
 interface CompanyDashboardTypes {
-  companyId: string;
+  companyId: number | undefined;
 }
 
 const CompanyDashboard = ({ companyId }: CompanyDashboardTypes) => {
   const assets = useSelector((state: ReducerTypes) =>
-    state.assets.filter((a) => a.companyId.toString() === companyId)
+    state.assets.filter((a) => a.companyId === companyId)
   );
   const units = useSelector((state: ReducerTypes) =>
-    state.units.filter((un) => un.companyId.toString() === companyId)
+    state.units.filter((un) => un.companyId === companyId)
   );
   const users = useSelector((state: ReducerTypes) =>
-    state.users.filter((u) => u.companyId.toString() === companyId)
+    state.users.filter((u) => u.companyId === companyId)
   );
+  const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
+
+  const options: Highcharts.Options = {
+    title: {
+      text: "Assets",
+    },
+    series: [
+      {
+        type: "line",
+        data: assets.map((a) => a.healthscore),
+      },
+    ],
+  };
 
   console.log(assets);
   console.log(units);
   console.log(users);
 
-  return <>CompanyDashboard</>;
+  return (
+    <HighchartsReact
+      highcharts={Highcharts}
+      options={options}
+      ref={chartComponentRef}
+    />
+  );
 };
